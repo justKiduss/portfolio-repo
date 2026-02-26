ab00cba5ca87e0d3616c5bb2884a1e5428b9a8b0
justkiduss <kidusteshome036@gmail.com>
2026-02-26 03:02:34 +0300

    first commit

---
+++ Games/canvas/server.js
@@ -0,0 +1,51 @@
+const canvas=document.querySelector("canvas");
+const ctx=canvas.getContext('2d');
+
+
+canvas.width=innerWidth; // prop of window object and it doesn't need windew.innerWidth
+canvas.height=innerHeight;
+
+class Player{
+    constructor(x,y,radius,color){
+        this.x=x;
+        this.y=y;
+        this.radius=radius;
+        this.color=color;
+    }
+    draw(){
+        ctx.beginPath();
+        ctx.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
+        ctx.fillStyle=this.color
+        ctx.fill()
+    }
+}
+
+class Projectile{
+    constructor(x,y,radius,color,velocity){
+        this.x=x;
+        this.y=y;
+        this.radius=radius;
+        this.color=color;
+        this.velocity=velocity;
+    }
+    draw(){
+        ctx.beginPath();
+        ctx.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
+        ctx.fillStyle=this.color
+        ctx.fill()
+    }
+}
+const x=canvas.width/2;
+const y=canvas.height/2;
+const player=new Player(x,y,30,'red');
+player.draw();
+
+addEventListener('click',(e)=>{
+    const x=e.clientX;
+    const y=e.clientY;
+
+    const projectile=new Projectile(x,y,5,'blue',null);
+    console.log(e)
+    projectile.draw();
+
+})
