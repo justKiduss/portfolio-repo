a3131bdce814da82df7c2351a6765509dc11c79d
justkiduss <kidusteshome036@gmail.com>
2026-03-05 17:55:10 +0300

    grid game

--- Games/Grid_Game/index.js
+++ Games/Grid_Game/index.js
@@ -1,35 +1,9 @@
 const canvas=document.querySelector("canvas");
-const ctx=canvas.getContext("2d");
+
+const ctx=canvas.getContext('2d');
 
 canvas.width=innerWidth;
 canvas.height=innerHeight;
-
-const player1Image=new Image();
-const player2Image=new Image();
-
-let loaded=0;
-function startGame(){
-    loaded++;
-    if(loaded === 2){
-        init();
-    }
-}
-
-player1Image.onload = startGame;
-player2Image.onload = startGame;
-
-
-player1Image.src="./asset/player1.jpg";
-player2Image.src="./asset/player2.jpg";
-
-
-
-function init(){
-
-const players=[
-    {id:"p1",row:0,col:0,image:player1Image},
-    {id:"p2",row:7,col:7,image:player2Image}
-]
 
 const matrix=[
     [1,0,1,0,1,0,1,0],
@@ -42,42 +16,103 @@ const matrix=[
     [0,1,0,1,0,1,0,1],
 ]
 
-class Grid{
-    constructor(matrix,size){
-        this.matrix=matrix;
-        this.rows=matrix.length;
-        this.cols=matrix[0].length;
-        this.size=size;
-        this.cellSize=size/this.rows;
+
+const player1=new Image();
+player1.src="./asset/player1.jpg";
+const player2=new Image();
+player2.src="./asset/player2.jpg";
+
+let loaded=0;
+function startGame(){
+    loaded++;
+    if(loaded==2){
+        init();
     }
-    draw(x,y){
-        for(let row=0;row<this.rows;row++){
-            for(let col=0;col<this.cols;col++){
-                const cellX = x + (col * this.cellSize);
-                const cellY = y + (row * this.cellSize);
+}
+
+player1.onload=startGame;
+player2.onload=startGame;
 
 
-                ctx.fillStyle=this.matrix[row][col]>0? "red":"black";
-                ctx.fillRect(cellX,cellY,this.cellSize,this.cellSize);
+state = {
+  board: {
+    rows: 8,
+    cols: 8
+  },
+
+  players: {
+    p1: {
+      id: "p1",
+      row: 0,
+      col: 0,
+      health: 3
+    },
+
+    p2: {
+      id: "p2",
+      row: 7,
+      col: 7,
+      health: 3
+    }
+  },
+
+  turn: "p1",
+
+  status: "playing"
+}
+
+
+function init(){
+    class Board{
+
+        constructor(boardHeight,boardwidth,x,y){
+            this.boardHeight=boardHeight;
+            this.boardwidth=boardwidth;
+            this.x=x;
+            this.y=y;
+        }
+
+        drawBoard(ctx){
+            ctx.fillStyle="black";
+            ctx.fillRect(this.x,this.y,this.boardwidth,this.boardHeight);
+        }
+
+        drawGrid(matrix,state,ctx){
+            const tileWidth=this.boardwidth/state.board.rows;
+            const tileHeight=this.boardHeight/state.board.cols;
+            for(let row=0;row<state.board.rows;row++){
+                for(let col=0;col<state.board.cols;col++){
+                    ctx.fillStyle=matrix[row][col]>0?"red":"black";
+
+                    const tilex=this.x+(col*tileWidth);
+                    const tiley=this.y+(row*tileHeight);
+
+                    ctx.fillRect(tilex,tiley,tileWidth,tileHeight);
+                }
             }
         }
+
+        drawPlayer(state,ctx){
+            const tileWidth=this.boardwidth/state.board.rows;
+            const tileHeight=this.boardHeight/state.board.cols;
+            const p1x=this.x+(state.players.p1.col*tileWidth);
+            const p1y=this.y+(state.players.p1.row*tileHeight);
+            const p2x=this.x+(state.players.p2.col*tileHeight);
+            const p2y=this.y+(state.players.p2.row*tileHeight);
+            ctx.drawImage(player1,p1x,p1y,tileWidth,tileHeight);
+            ctx.drawImage(player2,p2x,p2y,tileWidth,tileHeight);
+        }
+
     }
-}
+    const boardwidth=500;
+    const boardHeight=500;
+    const x=canvas.width/2-boardwidth/2;
+    const y=canvas.height/2-boardHeight/2;
 
-function drawPlayers(x,y,cellSize){
-    players.forEach(player=>{
-        const px=x+ (player.col*cellSize);
-        const py=y+ (player.row*cellSize);
+    const board=new Board(boardHeight,boardwidth,x,y);
+    board.drawBoard(ctx);
+    board.drawGrid(matrix,state,ctx);
+    board.drawPlayer(state,ctx);
+    console.log(ctx);
 
-        ctx.drawImage(player.image,px,py,cellSize,cellSize)
-    })
-}
-
-const x=innerWidth/2-(500/2);
-const y=innerHeight/2-(500/2);
-
-const grid=new Grid(matrix,500);
-
-grid.draw(x,y);
-drawPlayers(x,y,grid.cellSize)
 }
