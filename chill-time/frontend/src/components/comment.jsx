8a9bd5908263d372932d5e2a0004f55d8c73093d
justkiduss <kidusteshome036@gmail.com>
2026-04-22 23:27:58 +0300

    deployment issue

--- chill-time/frontend/src/components/comment.jsx
+++ chill-time/frontend/src/components/comment.jsx
@@ -14,11 +14,13 @@ export default function Comment({ movieId, moviename }) {
     const reviews = useReview(state, dispatch);
     // const [loading, setLoading] = useState(false);
 
+    const hydrate=reviews.hydrate;
+
     useEffect(()=>{
         if (state.loading) return;
         if (state.allIds.length > 0) return;
-        reviews.hydrate(movieId);
-    },[movieId])
+        hydrate(movieId);
+    },[movieId,state.loading,state.allIds.length])
 
     // CREATE
     async function handleSubmit(e) {
