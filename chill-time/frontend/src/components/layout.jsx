9c30599994a9a2ee3fed1d68007fc83236210366
justkiduss <kidusteshome036@gmail.com>
2026-04-24 05:25:58 +0300

    theme styles

--- chill-time/frontend/src/components/layout.jsx
+++ chill-time/frontend/src/components/layout.jsx
@@ -164,7 +164,7 @@ export default function Layout() {
           </div>
         </div>
         <ThemeToggle/>
-        <Link to="/login" className="dark:text-white">
+        <Link to="/login" className="hidden md:block dark:text-white">
           <CircleUser size={24} />
         </Link>
       </header>
