967502e31f1d7653f64ff0251f4da9f4adac69ba
justkiduss <kidusteshome036@gmail.com>
2026-05-09 00:01:18 +0300

    fecthing commits

--- portfolio/backend/server.ts
+++ portfolio/backend/server.ts
@@ -1,15 +1,26 @@
-import express, {Request,Response} from "express";
+import type {Request,Response} from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 
+dotenv.config();
+
+import express from "express";
+import router from "./routes/githubRoutes";
 const app =express();
-const port=5000;
+const port=process.env.PORT || 8000;
 
 app.use(express.json());
+app.use(cors({
+    origin:"*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
-app.get("/",(req:Request,res:Response)=>{
+app.get("/api",(req:Request,res:Response)=>{
     res.send("Api is working");
 })
-
+app.use("/api",router);
 app.listen(port,()=>{
-    console.log(`Server running in the ${port}`);
+    console.log(`Server running in the http://localhost:${port}`);
 })
 
