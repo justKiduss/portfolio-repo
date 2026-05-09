7b9f31e0027725ebbf3a3f2b806735b4547ee419
justkiduss <kidusteshome036@gmail.com>
2026-05-10 00:15:54 +0300

    integreting with frontend

--- portfolio/backend/routes/githubRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { githubActivity } from "../controller/github";
-
-const router = express.Router();
-
-router.get('/github-activity', githubActivity);
-
-export default router;
--- portfolio/portfolio-site/src/service/gitservice.ts
+++ portfolio/portfolio-site/src/service/gitservice.ts
@@ -1,6 +1,6 @@
 export default async function GitService(){
     try{
-        const response=await fetch('http://localhost:8000/api/github-activity');
+        const response=await fetch('https://portfolio-psi-pink-as9zdg26th.vercel.app/api/github-activity');
         console.log("respnse",response);
         if(!response.ok) throw new Error("Server not responding");
         return await response.json();
