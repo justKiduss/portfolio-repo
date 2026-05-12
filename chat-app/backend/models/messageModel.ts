6f6924d432043035b2c67f17afba30500d1c197d
justkiduss <kidusteshome036@gmail.com>
2026-05-12 04:42:59 +0300

    in bacend added model and service for users and message

---
+++ chat-app/backend/models/messageModel.ts
@@ -0,0 +1,55 @@
+import pool from "../config/db";
+const messageModel=()=>{
+    interface CreateMessageDTO {
+        senderId: number;
+        receiverId: number;
+        text: string;
+        image?: string;
+    }
+    interface updateMessage{
+        text:string;
+        image?:string;
+    }
+    return{
+        getAllMessages:async()=>{
+            const res=await pool.query('SELECT * from message');
+            return res.rows;
+        },
+        getById:async(id:number)=>{
+            const res=await pool.query(`SELECT * from message WHERE id=$1`,[id]);
+            return res.rows[0];
+        },
+        create:async(data:CreateMessageDTO)=>{
+            const {senderId,receiverId,text,image}=data;
+            const res=await pool.query(`INSERT INTO message (senderId,receiverId,text,image) VALUES ($1,$2,$3,$4) RETURNING *`,
+                [senderId,receiverId,text,image]);
+            return res.rows[0];
+        },
+        update:async(id:number,data:updateMessage)=>{
+            const {text,image}=data;
+            const res=await pool.query(`UPDATE message SET text=$1,image=$2 where id=$3 RETURNING *`,
+                [text,image,id]
+            );
+            return res.rows[0];
+        },
+        delete:async(id:number)=>{
+            const res=await pool.query('DELETE from message WHERE id=$1 RETURNING *',[id]
+            );
+            return res.rows[0];
+        },
+        getMessagesByUserId:async(userId:number)=>{
+            const res=await pool.query(`SELECT * from message WHERE senderId=$1 or receiverId=$1`,[userId]
+            )
+            return res.rows
+        },
+        getConversationMessages:async(senderId:number,receiverId:number)=>{
+            const res=await pool.query(`SELECT * from message WHERE 
+                (senderId=$1 AND receiverId=$2)
+                or (receiverId=$1 AND senderId=$2) ORDER BY created_at ASC`,[senderId,receiverId])
+                return res.rows;
+        }
+    }
+}
+
+const model=messageModel;
+export default model;
---
+++ chat-app/backend/models/userModel.ts
@@ -0,0 +1,52 @@
+import pool from "../config/db";
+export function userModel(){
+    interface CreateUserDTO {
+        username: string;
+        email: string;
+        password: string;
+        profilePic?: string;
+        isAdmin?: boolean;
+    }
+    return{
+        getAllUsers:async()=>{
+            const res=await pool.query('SELECT * from users');
+            return res.rows;
+        },
+        getById:async(id: number)=>{
+            const res=await pool.query('SELECT * from users WHERE id=$1',[id]
+            );
+            return res.rows[0];
+        },
+        getByUsername:async(username: string)=>{
+            const res=await pool.query('SELECT * from users WHERE username=$1',[username]);
+            return res.rows[0];
+        },
+        create:async(data:CreateUserDTO)=>{
+            const {username,email,password,profilePic,isAdmin}=data;
+            const res=await pool.query(`INSERT INTO users (username,email,password,profilePic,isAdmin)
+                VALUES ($1,$2,$3,$4,$5) RETURNING *`,[username,email,password,profilePic,isAdmin]
+            );
+            return res.rows[0];
+        },
+        getByEmail:async(email:string)=>{
+            const res=await pool.query('SELECT * from users WHERE email=$1',[email]   
+            );
+            return res.rows[0];
+        },
+        update:async(id:number,data:CreateUserDTO)=>{
+            const {username,email,password,profilePic,isAdmin}=data;
+            const res=await pool.query(`UPDATE users SET username=$1,email=$2,password=$3,profilePic=$4,isAdmin=$5,updated_at=NOW() WHERE id=$6 RETURNING *,
+                [username,email,password,profilePic,isAdmin,id]`
+            );
+            return res.rows[0];
+        },
+        delete:async (id:number)=>{
+            const res=await pool.query(`DELETE from users WHERE id=$1 RETURNING *`,[id]
+            );
+            return res.rows[0];
+        }
+    }
+}
+
+const model=userModel();
+export default model;
