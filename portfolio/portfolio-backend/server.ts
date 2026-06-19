c5dc87efbb5c4d4fcee08aa1370b0186027eae39
justkiduss <kidusteshome036@gmail.com>
2026-06-19 03:36:43 +0300

    changing the server from render to vercel

--- portfolio/portfolio-backend/server.ts
+++ portfolio/portfolio-backend/server.ts
@@ -7,7 +7,7 @@ import cors from "cors";
 import router from "./routes/githubRoutes";
 
 const app =express();
-const port=process.env.PORT || 8000;
+// const port=process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(cors({
