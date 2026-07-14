e07516b06b4a68888ae56126cca42d6c590fd270
justkiduss <kidusteshome036@gmail.com>
2026-07-14 10:41:56 +0300

    updating the model with the frontend data

--- chill-time/backend/models/watch_later.js
+++ chill-time/backend/models/watch_later.js
@@ -9,7 +9,7 @@ export function watch_later(){
         addMovie:async(data,user_id)=>{
             const {movieId,title,poster}=data;
             const query=`INSERT INTO watch_later (user_id,movie_id,movie_title,poster_path) 
-                    values ($1,$2,$3,$4,$5,$6) RETURNING *`;
+                    values ($1,$2,$3,$4) RETURNING *`;
             const values=[user_id,movieId,title,poster];
             const res=await pool.query(query,values);
             return res.rows[0];
